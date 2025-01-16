0cffff6ed4a84b3640339bee86f3f0712b28bbfb
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-16 15:59:25 -0500

    Created Gallery model, migrations, added serializers and urls page

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+  image = models.ImageField(upload_to='gallery/')
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
