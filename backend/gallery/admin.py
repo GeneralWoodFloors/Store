ff116b308e5f1c38004cd1729875917bc5bd3a2f
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-16 18:55:31 -0500

    Successfully created urls; added the gallery to the admin view allowing them to create, update and delete

--- backend/gallery/admin.py
+++ backend/gallery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Gallery
 
-# Register your models here.
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')  # Columns to display
+    search_fields = ('title', 'description')     # Fields to search
--- backend/gallery/urls.py
+++ backend/gallery/urls.py
@@ -3,11 +3,13 @@ from .views import GalleryView, SingleView, CreateGalleryView, UpdateGalleryView
 
 urlpatterns = [
     path ('', GalleryView.as_view(), name='view-gallery'),
-    path ('<id>/', SingleView.as_view(), name='image-detail'),
     path ('create/', CreateGalleryView.as_view(), name='create-image'),
-    path ('<id>/update/', UpdateGalleryView.as_view(), name='update-image'), 
-    path ('<id>/delete/', DeleteGalleryView.as_view(), name='delete-image')
+    path ('<int:pk>/', SingleView.as_view(), name='image-detail'),
+    path ('<int:pk>/update/', UpdateGalleryView.as_view(), name='update-image'), 
+    path ('<int:pk>/delete/', DeleteGalleryView.as_view(), name='delete-image')
 ]
 
 #<id> -> id 
 #<int:pk> -> primary key 
+
+#order in which the path is created matters
