54933f724a13b2f6e0064383fb22c5c58ac2d380
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-16 11:28:23 -0500

    Created urls for creating, and the getting the token and token refresh

---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import CreateUserView
+
+urlpatterns = [
+  path('register/', CreateUserView.as_view(), name='register'),
+  path('login/', TokenObtainPairView.as_view(), name="get_token"),
+  path('login/refresh', TokenRefreshView.as_view(), name="token_refresh"),
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/register/', CreateUserView.as_view(), name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
-    path('api/token/refresh', TokenRefreshView.as_view(), name="refresh"),
+    path('account/', include('accounts.urls')),
     path('api-auth/', include("rest_framework.urls")) 
 ]
