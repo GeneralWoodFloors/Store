3bda36b9c8e4e06729f130e9a4e1cf0dd0005a80
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-22 14:07:46 -0500

    Added CSS to the Gallery Page

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "../styles/Gallery.css"
 
 function Gallery() {
   const [images, setImages] = useState([]);
