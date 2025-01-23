d6397adf0d4420c79af799c40f740d32f3825df0
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-22 22:26:39 -0500

    Updated the Login route

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,9 @@
 import Form from "../components/Form"
-import { Link } from "react-router-dom"
 
 function Login() {
   return (
     <div>
       <Form route="account/login/" method="login"/>
-
-      <Link to="/admin" className="button-link-Admin"> 
-      <button>Are you Admin?</button>
-      </Link>
     </div>
 )
 }
