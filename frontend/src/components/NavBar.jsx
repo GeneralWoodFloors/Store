98e4a44969828008f1a1d24fb60c4076e05c5c1f
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-22 19:05:03 -0500

    Added Profile in NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         <Link to="/gallery" className="link">Gallery</Link>
         {/* <Link to="/booking" className="link">Booking</Link> */}
         <Link to="/payment" className="link">Payment</Link> 
+        <Link to="/user" className="link">Profile</Link>
       </div>
     </nav>
   );
