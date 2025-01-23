912b251cff9df0198583999d76661d24407aad75
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-22 22:54:10 -0500

    Added the Booking page to the NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <Link to="/" className="link">Home</Link>
         <Link to="/about" className="link">About Us</Link>
         <Link to="/gallery" className="link">Gallery</Link>
-        {/* <Link to="/booking" className="link">Booking</Link> */}
+        <Link to="/booking" className="link">Booking</Link>
         <Link to="/payment" className="link">Payment</Link> 
         <Link to="/user" className="link">Profile</Link>
         <li><a href="http://127.0.0.1:8000/admin" target="_blank" rel="noopener noreferrer">Admin</a></li>
