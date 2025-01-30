caa61d31926d5a368ba1f03805a233d391148ad9
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-01-30 14:14:55 -0500

    Created booking model

--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField()
+    event_uri = models.CharField(max_length=255)
+    event_id = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.email} - {self.event_uri}"
