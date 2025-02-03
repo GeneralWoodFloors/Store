57b7adfdd548c575995c4549d5bb592f04b0e5c4
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-03 12:27:10 -0500

    Allowing admin to magage contact messages

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "created_at", "responded")
+    list_filter = ("responded", "created_at")
+    search_fields = ("name", "email", "message")
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
