2d5f9194ca6dee22c4f3ed483cf265ed33eeab4e
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-05 22:25:57 -0500

    Added UserMessages to profile page & updated based on what is currently managable

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,4 +1,5 @@
 import Profile  from "../components/Profile"
+import UserMessages from "../components/UserMessages"
 
 function UserProfile() {
   return (
@@ -6,17 +7,8 @@ function UserProfile() {
       <div className="profile">
         <Profile/>
       </div>
-      <div className="appointments">
-        <h2>Upcoming Bookings</h2>
-        <p>No upcoming appointments.</p>
-      </div>
-      <div className="payments">
-        <h2>Upcoming Payments</h2>
-        <p>No upcoming payments.</p>
-      </div>
       <div className="messages">
-        <h2>Messages</h2>
-        <p>No messages from Admin</p>
+        <UserMessages/>
       </div>
     </section>
 )
