ddcdb1cd0c019526e88ca58f31153fb70b2585d2
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-10 11:20:39 -0500

    Created Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+        <div className="footer-links">
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
