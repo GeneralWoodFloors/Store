5fed20f7609ac712a170e820b96f430a0bd2ca8b
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-10 11:57:08 -0500

    Created Footer css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "../styles/Footer.css"
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} General Wood Floors. All rights reserved.</p>
         <div className="footer-links">
           <Link to="/about">About Us</Link>
           <Link to="/contact">Contact</Link>
