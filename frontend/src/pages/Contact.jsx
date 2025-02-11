c663cfe3c275d3ca7f0e778d0cbaa9d7d24ce903
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-11 17:14:04 -0500

    Added more css to Contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -4,14 +4,15 @@ import "../styles/Contact.css"
 function ContactUs () {
   return (
     <div className="contact-page">
-      <section className="Contact">
-        <ContactForm/>
-      </section>
-      <section className="contact-details">
+        <section className="contact-details">
         <h1>Contact Us</h1>
         <p>Located at Queens, NY</p>
         <p>Phone number: (347)-324-2813</p>
         <p>Email: generalwoodfloors@gmail.com</p>
+      </section>
+
+      <section className="Contact">
+        <ContactForm/>
       </section>
     </div>
   );
