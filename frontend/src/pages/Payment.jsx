7a76a041b88823ac78d0eb77b36f1d1f0a176238
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-12 16:52:58 -0500

    Added icons for payments

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/Payment.css"
+import checkIcon from "../assets/check-icon.jpg"
+import zelleIcon from "../assets/Zelle-icon.png"
+import cashIcon from "../assets/cash-icon.png"
 
 const Payment = () => {
   return (
@@ -9,9 +12,9 @@ const Payment = () => {
         We offer the following payment methods for your convenience:
       </p>
       <ul className="payment-list">
-        <li>Zelle </li>
-        <li>Check </li>
-        <li>Cash</li>
+        <li><img src={zelleIcon} alt="Zelle icon" className="payment-icon" /> Zelle</li>
+        <li><img src={checkIcon} alt="Check icon" className="payment-icon" /> Check</li>
+        <li><img src={cashIcon} alt="Cash icon" className="payment-icon" /> Cash</li>
       </ul>
       <p className="note">
         An 8% tax will be added for Zelle and Check payments unless the customer opts for regular prices.
