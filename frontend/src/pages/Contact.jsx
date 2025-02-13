7f766c3114cbd5d400a7d42271438183372a54a7
Sleepy-kookoo <americaxalvarez@gmail.com>
2025-02-12 21:48:26 -0500

    Added slogan and mini img

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -5,21 +5,28 @@ import popeyeIcon from "../assets/popeye.png";
 function ContactUs() {
   return (
     <div className="contact-page">
-      <section className="contact-details">
-        <h1>Contact Us</h1>
-        <p>Located at Queens, NY</p>
-        <p>Phone number: (347)-324-2813</p>
-        <p>Email: generalwoodfloors@gmail.com</p>
-      </section>
-      
-      <section className="slogan">
-        {/* <img src={popeyeIcon} alt="Popeye-sailer-man" /> */}
-        {/* <p>Quality You Can Count On</p> */}
-      </section>
+      <div className="contact-content">
+        {/* Contact Details and Slogan Wrapper */}
+        <div className="contact-left">
+          <section className="contact-details">
+            <h1>Contact Us</h1>
+            <p>Located at Queens, NY</p>
+            <p>Phone number: (347)-324-2813</p>
+            <p>Email: generalwoodfloors@gmail.com</p>
+          </section>
 
-      <section className="Contact">
-        <ContactForm/>
-      </section>
+          {/* Slogan Section */}
+          <section className="slogan">
+            <img src={popeyeIcon} alt="Popeye-sailer-man" />
+            <p>Quality You Can Count On!</p>
+          </section>
+        </div>
+
+        {/* Contact Form */}
+        <section className="Contact">
+          <ContactForm />
+        </section>
+      </div>
     </div>
   );
 }
